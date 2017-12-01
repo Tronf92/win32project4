eb04cf68bbba347b3b7e005c8a04370d2f792bc0
na <ady.niko51@gmail.com>
2017-12-01 19:25:42 +0200

    first commit

---
+++ Win32Project4/stdafx.cpp
@@ -0,0 +1,9 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Win32Project4.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+#include "MyForm.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
